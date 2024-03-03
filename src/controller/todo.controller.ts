c6879a9081503f0217b327b8e3334638deac3bf3
Rajat Khurana <rajat_khurana@epam.com>
2024-03-03 11:04:11 +0530

    changes

--- src/controller/todo.controller.ts
+++ src/controller/todo.controller.ts
@@ -1,33 +1,43 @@
+import { handleError } from "../helpers/error";
 import { getRequest } from "../utils/axios";
+import { JSON_API_URL } from "../utils/constants";
 
 export class Todo {
 
     constructor() { }
 
     static async fetchToDos(_req: any, res: any) {
-        const todos: Array<any> = [];
+        try {
+            const todos: Array<any> = [];
+            
+            // Fetch the first 20 even-numbered TODOs
+            for (let i = 2; i <= 40; i += 2) {
+                const response = await getRequest(`${JSON_API_URL}/${i}`);
+                todos.push(response.data);
+            }
+            for (const todo of todos) {
+                const completed = todo.completed ? 'Completed' : 'Incomplete';
+                todo.completed = completed;
+            }
 
-        // Fetch the first 20 even-numbered TODOs
-        for (let i = 2; i <= 40; i += 2) {
-            const response = await getRequest(`https://jsonplaceholder.typicode.com/todos/${i}`);
-            todos.push(response.data);
+            res.send(todos);
+        } catch (error) {
+            handleError(error, res);
         }
-        for (const todo of todos) {
-            const completed = todo.completed ? 'Completed' : 'Incomplete';
-            todo.completed = completed;
-        }
-
-        res.send(todos);
     }
 
     static async fetchSpecificToDo(req: any, res: any) {
-        const id: number | string = req.params.id;
-        const response = await getRequest(`https://jsonplaceholder.typicode.com/todos/${id}`);
-        const completed = response?.data?.completed ? "Completed" : "Incomplete";
-        res.send({
-            ...response?.data,
-            completed,
-        });
+        try {
+            const id: number | string = req.params.id;
+            const response = await getRequest(`${JSON_API_URL}/${id}`);
+            const completed = response?.data?.completed ? "Completed" : "Incomplete";
+            res.send({
+                ...response?.data,
+                completed,
+            });
+        } catch (error) {
+            handleError(error, res);
+        }
     }
 
 };
